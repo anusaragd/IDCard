b478cddec02976d5c5578f48949144839456a891
MASTERS <anusara.orm@gmail.com>
2017-12-12 16:49:16 +0700

    add login

--- app/src/main/java/com/example/masters/idcard/MainActivity.java
+++ app/src/main/java/com/example/masters/idcard/MainActivity.java
@@ -5,17 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText Username,Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
+
+        Username = (EditText)findViewById(R.id.username);
+        Password = (EditText)findViewById(R.id.password);
+
+        button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
