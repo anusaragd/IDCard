603e8804d67530617fd058a7115de9295345057c
MASTERS <anusara.orm@gmail.com>
2017-12-13 13:57:54 +0700

    add create room

--- app/src/main/java/com/example/masters/idcard/CreateActivity.java
+++ app/src/main/java/com/example/masters/idcard/CreateActivity.java
@@ -2,12 +2,16 @@ package com.example.masters.idcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CreateActivity extends AppCompatActivity {
+
+    EditText nameroome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
     }
 }
