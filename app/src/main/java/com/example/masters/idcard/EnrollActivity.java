fa405d39d707a42899801ade98ae67f3b8d0470e
MASTERS <anusara.orm@gmail.com>
2017-12-14 14:00:10 +0700

    add id

--- app/src/main/java/com/example/masters/idcard/EnrollActivity.java
+++ app/src/main/java/com/example/masters/idcard/EnrollActivity.java
@@ -2,12 +2,24 @@ package com.example.masters.idcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EnrollActivity extends AppCompatActivity {
+
+    Button read , save;
+    EditText idcard , nameTH , nameENG , Bday , dateofissue, dateofexpiry;
+    ImageView picidcard;
+    TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
+
+
+
     }
 }
